8156abc924e62e0f8b463ea32f74609187da44f8
Anatolius95 <valenta.tomas95@gmail.com>
2016-07-14 14:22:06 +0800

    Design

--- app/src/main/java/cz/uhk/basketballcounter/MainActivity.java
+++ app/src/main/java/cz/uhk/basketballcounter/MainActivity.java
@@ -1,5 +1,6 @@
 package cz.uhk.basketballcounter;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -16,34 +17,58 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    protected void color () {
+        TextView pocitadloA = (TextView) findViewById (R.id.pocitadloA);
+        TextView pocitadloB = (TextView) findViewById (R.id.pocitadloB);
+
+        if (PocitadloA > PocitadloB) {
+            pocitadloA.setBackgroundColor(Color.parseColor("#0AAD00"));
+            pocitadloB.setBackgroundColor(Color.parseColor("#AD2100"));
+        } else {
+            if (PocitadloA < PocitadloB) {
+                pocitadloA.setBackgroundColor(Color.parseColor("#AD2100"));
+                pocitadloB.setBackgroundColor(Color.parseColor("#0AAD00"));
+            } else {
+                pocitadloA.setBackgroundColor(Color.parseColor("#BFBDBB"));
+                pocitadloB.setBackgroundColor(Color.parseColor("#BFBDBB"));
+            }
+        }
+    }
+
     public void plusTwoA (View view) {
         PocitadloA = PocitadloA + 2;
         displayA(PocitadloA);
+        color();
     }
 
     public void plusThreeA (View view) {
         PocitadloA = PocitadloA + 3;
         displayA(PocitadloA);
+        color();
     }
 
     public void plusFreeA (View view) {
         PocitadloA = PocitadloA + 1;
         displayA(PocitadloA);
+        color();
     }
 
     public void plusTwoB (View view) {
         PocitadloB = PocitadloB + 2;
         displayB(PocitadloB);
+        color();
     }
 
     public void plusThreeB (View view) {
         PocitadloB = PocitadloB + 3;
         displayB(PocitadloB);
+        color();
     }
 
     public void plusFreeB (View view) {
         PocitadloB = PocitadloB + 1;
         displayB(PocitadloB);
+        color();
     }
 
     public void reset (View view) {
@@ -51,6 +76,7 @@ public class MainActivity extends AppCompatActivity {
         PocitadloB = 0;
         displayA(PocitadloA);
         displayB(PocitadloB);
+        color();
     }
 
     private void displayA(int number) {
